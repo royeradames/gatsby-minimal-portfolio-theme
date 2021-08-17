8ec803c88d5eb1e9eac0d2c304506ae81f58211d
royeradames <royeraadames@gmail.com>
2021-08-17 18:40:14 -0400

    📦wrap page with Layout comp

--- src/pages/contact-form.tsx
+++ src/pages/contact-form.tsx
@@ -7,6 +7,7 @@ import axios from "axios"
 
 //components
 import SocialMedia from "../components/social-media"
+import Layout from "../components/Layout"
 
 // schema
 const schema = yup.object().shape({
@@ -45,102 +46,104 @@ const ContactForm = () => {
   }
 
   return (
-    <main className="website__pages contact-page">
-      <article className="contact-message">
-        <div className="horizontal-line contact-message__horizontal-line " />
-        <h2 className="title contact-message__title">Get in Touch</h2>
-        <p className="message-s contact-message__message">
-          I’d love to hear about what you’re working on and how I could help.
-          I’m currently looking for a new role and am open to a wide range of
-          opportunities. My preference would be to find a position in a company
-          in London. But I’m also happy to hear about opportunites that don’t
-          fit that description. I’m a hard-working and positive person who will
-          always approach each task with a sense of purpose and attention to
-          detail. Please do feel free to check out my online profiles below and
-          get in touch using the form.
-        </p>
-        <SocialMedia page="contact-message" />
-      </article>
-      <article className="contact-form">
-        <div className="horizontal-line contact-form__horizontal-line " />
-        <h2 className="title contact-form__title">Contact Me</h2>
-        <form onSubmit={handleSubmit(onSubmit)} className="contact-form__form">
-          {/* 
-            Name
-            email
-            message
-            submit
-            */}
-          {/* Name */}
-          <div className="form-elements">
-            <label
-              htmlFor="name"
-              className="form-elements__label contact-form__label"
-            >
-              Name
-            </label>
-            <input
-              {...register("name")}
-              className={`form-elements__input contact-form__name ${
-                errors.name ? "form-elements__error-input" : ""
-              }`}
-              placeholder="Jane Appleseed"
-            />
-            <p className="form-elements__error-message">
-              {errors.name?.message}
-            </p>
-          </div>
-
-          {/* email */}
-          <div className="form-elements">
-            <label
-              htmlFor="email"
-              className="form-elements__label contact-form__label contact-form__label-email"
-            >
-              Email
-            </label>
-            <input
-              {...register("email")}
-              className={`form-elements__input contact-form__email ${
-                errors.email ? "form-elements__error-input" : ""
-              }`}
-              placeholder="email@example.com"
-            />
-            <p className="form-elements__error-message ">
-              {errors.email?.message}
-            </p>
-          </div>
-
-          {/* message */}
-          <div className="form-elements">
-            <label
-              htmlFor="message"
-              className={`contact-form__input-label contact-form__label-message `}
-            >
-              Message
-            </label>
-            <input
-              {...register("message")}
-              className={`form-elements__input  form-elements__input-message ${
-                errors.message ? "form-elements__error-input" : ""
-              }`}
-              placeholder="How can I help?"
-            />
-            <p className="form-elements__error-message ">
-              {errors.message?.message}
-            </p>
-          </div>
-
-          {/* submit */}
-          <button
-            type="submit"
-            className=" button button-primary-normal button__text form-elements__submit"
+    <Layout>
+      <main className="website__pages contact-page">
+        <article className="contact-message">
+          <div className="horizontal-line contact-message__horizontal-line " />
+          <h2 className="title contact-message__title">Get in Touch</h2>
+          <p className="message-s contact-message__message">
+            I’d love to hear about what you’re working on and how I could help.
+            I’m currently looking for a new role and am open to a wide range of
+            opportunities. My preference would be to find a position in a
+            company in London. But I’m also happy to hear about opportunites
+            that don’t fit that description. I’m a hard-working and positive
+            person who will always approach each task with a sense of purpose
+            and attention to detail. Please do feel free to check out my online
+            profiles below and get in touch using the form.
+          </p>
+          <SocialMedia page="contact-message" />
+        </article>
+        <article className="contact-form">
+          <div className="horizontal-line contact-form__horizontal-line " />
+          <h2 className="title contact-form__title">Contact Me</h2>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="contact-form__form"
           >
-            Send Message
-          </button>
-        </form>
-      </article>
-    </main>
+            {/*
+              Name
+              email
+              message
+              submit
+              */}
+            {/* Name */}
+            <div className="form-elements">
+              <label
+                htmlFor="name"
+                className="form-elements__label contact-form__label"
+              >
+                Name
+              </label>
+              <input
+                {...register("name")}
+                className={`form-elements__input contact-form__name ${
+                  errors.name ? "form-elements__error-input" : ""
+                }`}
+                placeholder="Jane Appleseed"
+              />
+              <p className="form-elements__error-message">
+                {errors.name?.message}
+              </p>
+            </div>
+            {/* email */}
+            <div className="form-elements">
+              <label
+                htmlFor="email"
+                className="form-elements__label contact-form__label contact-form__label-email"
+              >
+                Email
+              </label>
+              <input
+                {...register("email")}
+                className={`form-elements__input contact-form__email ${
+                  errors.email ? "form-elements__error-input" : ""
+                }`}
+                placeholder="email@example.com"
+              />
+              <p className="form-elements__error-message ">
+                {errors.email?.message}
+              </p>
+            </div>
+            {/* message */}
+            <div className="form-elements">
+              <label
+                htmlFor="message"
+                className={`contact-form__input-label contact-form__label-message `}
+              >
+                Message
+              </label>
+              <input
+                {...register("message")}
+                className={`form-elements__input  form-elements__input-message ${
+                  errors.message ? "form-elements__error-input" : ""
+                }`}
+                placeholder="How can I help?"
+              />
+              <p className="form-elements__error-message ">
+                {errors.message?.message}
+              </p>
+            </div>
+            {/* submit */}
+            <button
+              type="submit"
+              className=" button button-primary-normal button__text form-elements__submit"
+            >
+              Send Message
+            </button>
+          </form>
+        </article>
+      </main>
+    </Layout>
   )
 }
 
