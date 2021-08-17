eafc20fac4ec3c57cf16ba163716b575b308f9ab
royeradames <royeraadames@gmail.com>
2021-08-17 18:39:51 -0400

    📦if page doesn't exist give users this content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,17 @@
 /* libraries */
 import * as React from "react"
 
+/* components */
+import Layout from "../components/Layout"
+
+/* if page doesn't exist give users this content */
 const NotFoundPage = () => (
-  <article>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </article>
+  <Layout>
+    <article>
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </article>
+  </Layout>
 )
 
 export default NotFoundPage
