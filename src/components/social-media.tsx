f9bf12cee40cabb020adbf5a53feabfa0baca40c
royeradames <royeraadames@gmail.com>
2021-08-21 17:19:14 -0400

    📦add social media link from site metadata📦add blog link

--- src/components/social-media.tsx
+++ src/components/social-media.tsx
@@ -1,34 +1,70 @@
 /* Labraries */
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 /* SVGs */
 import Github from "../images/icons/github.svg"
 import Twitter from "../images/icons/twitter.svg"
 import LinkedIn from "../images/icons/linkedin.svg"
+import Blog from "../images/icons/blog.svg"
 
 const SocialMedia = (props: { page?: string }) => {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          linkedIn
+          github
+          twitter
+          blog
+        }
+      }
+    }
+  `)
+  const { linkedIn, github, twitter, blog } = site.siteMetadata
+
+  const socialLink = (socialMedia: string) => {
+    /* convert the incoming key string to lowercase */
+    socialMedia = socialMedia.toLowerCase()
+
+    /* get the links from the sitemetadata */
+    // if not then set it to #root
+    switch (socialMedia) {
+      case "twitter":
+        return twitter || "#root"
+      case "github":
+        return github || "#root"
+      case "linkedin":
+        return linkedIn || "#root"
+      case "blog":
+        return blog || "#root"
+      default:
+        return "#root"
+    }
+  }
   return (
     <article
       className={`social-media ${
         props.page ? `${props.page}__social-media` : ""
       }`}
     >
-      <a href="https://github.com/royeradames" target="_blank" rel="noreferrer">
+      <a href={socialLink("github")} target="_blank" rel="noreferrer">
         <Github
           className={`social-media__icon social-media__github ${props.page}__social-media-icon`}
         />
       </a>
-      <a href="#root" target="_blank" rel="noreferrer">
+      <a href={socialLink("twitter")} target="_blank" rel="noreferrer">
         <Twitter
           className={`social-media__icon social-media__github ${props.page}__social-media-icon`}
         />
       </a>
-      <a
-        href="https://www.linkedin.com/in/royer-adames/"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href={socialLink("linkedin")} target="_blank" rel="noreferrer">
         <LinkedIn
+          className={`social-media__icon social-media__github ${props.page}__social-media-icon`}
+        />
+      </a>
+      <a href={socialLink("blog")} target="_blank" rel="noreferrer">
+        <Blog
           className={`social-media__icon social-media__github ${props.page}__social-media-icon`}
         />
       </a>
