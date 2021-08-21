fe75c0f736285b1ade489ad40071f14c88fa817d
royeradames <royeraadames@gmail.com>
2021-08-21 17:18:19 -0400

    📦add metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,18 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: "Minimal Portfolio",
+    description: "A light weight and simply design portfolio.",
+    author: "Royer Adames",
+    linkedIn: "https://www.linkedin.com/in/royer-adames/",
+    blog: "https://royeraadames.medium.com/",
+    github: "https://github.com/royeradames",
+    twitter: "",
+  },
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
